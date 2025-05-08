0e599359852fe1b9109be20ac87b5c97467be796
hlkim <hlkim@enki.co.kr>
2025-05-08 22:18:04 +0900

    ADD: new dev world

--- packages/client/src/app/login/page.tsx
+++ packages/client/src/app/login/page.tsx
@@ -3,10 +3,17 @@
 import React, { useEffect } from "react";
 import { useLoginCheck } from "@scspace-client/Apis/auth/useLoginCheck";
 import { useLinkPush } from "@scspace-client/Hooks/useLinkPush";
+import { randomBytes, createHash } from "crypto";
 
 const LoginPage: React.FC = () => {
   const { linkPush } = useLinkPush();
   const { isLogined } = useLoginCheck();
+
+  function randomString() {
+    const rnd = randomBytes(32).toString();
+    const hash = createHash("sha256").update(rnd).digest("hex");
+    return hash;
+  }
 
   useEffect(() => {
     if (isLogined) {
@@ -15,15 +22,51 @@ const LoginPage: React.FC = () => {
       return;
     }
 
-    const location = process.env.NEXT_PUBLIC_SS_URL
-      ? `${process.env.NEXT_PUBLIC_SS_URL}?${new Date().getTime()}&redirect_url=${encodeURI(
-          `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-        )}`
-      : "";
-    console.log("location", location);
+    const client_id = "kaist-scs";
+    const server_url = "https://sso.kaist.ac.kr/auth/user/single/login/authorize";
+    const redirect_uri = "https://scspace.kws.sparcs.net/api/auth/login";
+    const state = randomString();
+    const nonce = randomString();
+    
 
-    window.location.href = location;
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = server_url;
+
+    // input 요소들 생성 및 추가
+    const inputClientId = document.createElement('input');
+    inputClientId.type = 'hidden';
+    inputClientId.name = 'client_id';
+    inputClientId.value = client_id;
+    form.appendChild(inputClientId);
+
+    const inputRedirectUri = document.createElement('input');
+    inputRedirectUri.type = 'hidden';
+    inputRedirectUri.name = 'redirect_uri';
+    inputRedirectUri.value = redirect_uri;
+    form.appendChild(inputRedirectUri);
+
+    const inputState = document.createElement('input');
+    inputState.type = 'hidden';
+    inputState.name = 'state';
+    inputState.value = state;
+    form.appendChild(inputState);
+
+    const inputNonce = document.createElement('input');
+    inputNonce.type = 'hidden';
+    inputNonce.name = 'nonce';
+    inputNonce.value = nonce;
+    form.appendChild(inputNonce);
+
+    // // 폼을 body에 추가하고 submit
+    document.body.appendChild(form);
+    console.log(form);
+    form.submit();
+
+
   }, [isLogined, linkPush]);
+  console.log("isLogined", isLogined);
+  console.log("test");
 
   return (
     <div id="main">
--- packages/server/src/feature/auth/auth.service.ts
+++ packages/server/src/feature/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { JwtService, TokenExpiredError } from '@nestjs/jwt';
 import * as crypto from 'crypto';
 import { ConfigService } from '@nestjs/config';
@@ -18,50 +18,137 @@ export class AuthService {
     private configService: ConfigService,
   ) {}
 
-  async login(result: string, state: string, res): Promise<void> {
-    const secretKey = this.configService.get<string>('SECRETKEY') + state;
-    const keySpec = Buffer.from(secretKey.substring(80, 96), 'utf8');
-    const iv = Buffer.from(secretKey.substring(80, 96), 'utf8');
-    const userInfo: UserSSOType2022 = this.decrypt(result, keySpec, iv);
+  async login(state: string, code: string, res: any): Promise<void> {
+    // const secretKey = this.configService.get<string>('SECRETKEY') + state;
+    // const keySpec = Buffer.from(secretKey.substring(80, 96), 'utf8');
+    // const iv = Buffer.from(secretKey.substring(80, 96), 'utf8');
+    // const userInfo: UserSSOType2022 = this.decrypt(result, keySpec, iv);
 
-    try {
-      const data = userInfo;
+    // try {
+    //   const data = userInfo;
 
-      const payload = this.ssoToUser(data);
-      Logger.log('PAYLOAD');
-      Logger.log(JSON.stringify(payload));
-      const user = await this.userPublicService.findUserByKaistUid(
-        payload.kaistUID,
-      );
-      Logger.log('USER');
-      Logger.log(JSON.stringify(user));
+    //   const payload = this.ssoToUser(data);
+    //   Logger.log('PAYLOAD');
+    //   Logger.log(JSON.stringify(payload));
+    //   const user = await this.userPublicService.findUserByKaistUid(
+    //     payload.kaistUID,
+    //   );
+    //   Logger.log('USER');
+    //   Logger.log(JSON.stringify(user));
 
-      const createdUser = this.muserToUser(
-        !user ? await this.userPublicService.insertUser(payload) : user,
-      );
-      Logger.log('CREATED USER');
-      Logger.log(JSON.stringify(createdUser));
-      const token = this.jwtService.sign(createdUser, {
-        expiresIn: '7d',
-        issuer: 'scspace',
-        subject: 'userInfo',
+    //   const createdUser = this.muserToUser(
+    //     !user ? await this.userPublicService.insertUser(payload) : user,
+    //   );
+    //   Logger.log('CREATED USER');
+    //   Logger.log(JSON.stringify(createdUser));
+    //   const token = this.jwtService.sign(createdUser, {
+    //     expiresIn: '7d',
+    //     issuer: 'scspace',
+    //     subject: 'userInfo',
+    //   });
+    //   Logger.log('TOKEN');
+    //   Logger.log(token);
+
+    //   res.cookie('scspacetoken1', token, {
+    //     maxAge: 60 * 60 * 1000 * 24 * 7,
+    //     secure: true,
+    //     sameSite: 'none',
+    //     httpOnly: true,
+    //     path: '/',
+    //   });
+
+    //   res.redirect(this.configService.get<string>('NEXT_PUBLIC_APP_URL') + '/');
+    // } catch (error) {
+    //   console.error(error);
+    //   res.status(500).send('Internal Server Error');
+    // }
+
+    if (!code) return;
+
+    // state => CSRF check 1
+
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+    const redirectUri = process.env.REDIRECT;
+    const serverApiUrl = process.env.USER_INFO_DEV;
+
+    const getUrlParams = (key: string): string | null => {
+      return new URLSearchParams(window.location.search).get(key);
+    };
+
+    const fetchUserData = async () => {
+      const body = new URLSearchParams({
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: code,
+        redirect_uri: redirectUri,
       });
-      Logger.log('TOKEN');
-      Logger.log(token);
 
-      res.cookie('scspacetoken1', token, {
-        maxAge: 60 * 60 * 1000 * 24 * 7,
-        secure: true,
-        sameSite: 'none',
-        httpOnly: true,
-        path: '/',
-      });
+      try {
+        const response = await fetch(serverApiUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-formurlencoded;charset=utf-8',
+          },
+          body: body.toString(),
+        });
 
-      res.redirect(this.configService.get<string>('NEXT_PUBLIC_APP_URL') + '/');
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
+        if (!response.ok) throw new Error(`HTTP error ${response.status}`);
+
+        const result = await response.json();
+
+        // if (result.errorCode) {
+        //   // setErrorCode(result.errorCode);
+        // } else if (result.nonce === sesNonce) {
+        //   const user = JSON.parse(result.userInfo);
+        //   setUserInfo(user);
+        //   console.log('User Mobile:', user.user_mbtlnum);
+        // }
+
+        const userInfo = result?.userInfo;
+        const resNonce = result?.nonce; // CSRF check 2
+
+        // ===== a parts of original code
+
+        const data = userInfo;
+
+        const payload = this.ssoToUser(data);
+        Logger.log('PAYLOAD');
+        Logger.log(JSON.stringify(payload));
+        const user = await this.userPublicService.findUserByKaistUid(
+          payload.kaistUID,
+        );
+        Logger.log('USER');
+        Logger.log(JSON.stringify(user));
+
+        const createdUser = this.muserToUser(
+          !user ? await this.userPublicService.insertUser(payload) : user,
+        );
+        Logger.log('CREATED USER');
+        Logger.log(JSON.stringify(createdUser));
+        const token = this.jwtService.sign(createdUser, {
+          expiresIn: '7d',
+          issuer: 'scspace',
+          subject: 'userInfo',
+        });
+        Logger.log('TOKEN');
+        Logger.log(token);
+
+        res.cookie('scspacetoken1', token, {
+          maxAge: 60 * 60 * 1000 * 24 * 7,
+          secure: true,
+          sameSite: 'none',
+          httpOnly: true,
+          path: '/',
+        });
+
+        res.redirect(
+          this.configService.get<string>('NEXT_PUBLIC_APP_URL') + '/',
+        );
+      } catch (error) {
+        console.error('API 통신 오류:', error);
+      }
+    };
   }
 
   async verification(cookies: any, res: Response): Promise<IUser | null> {
