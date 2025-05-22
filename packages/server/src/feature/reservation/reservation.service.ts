c55a9b1632c2adeaadb4ffb93aa171d3ea2298c9
h1ghl1kh7 <kimhyilim.915@gmail.com>
2025-05-23 08:04:17 +0900

    UPDATE: back

--- packages/server/src/feature/reservation/reservation.service.ts
+++ packages/server/src/feature/reservation/reservation.service.ts
@@ -38,11 +38,14 @@ export class ReservationService {
     timeTo?: number,
   ): Promise<IReservationAll[]> {
 
+    console.log(timeFrom, timeTo);
     if (timeFrom && timeTo) {
-      if (timeFrom >= timeTo) throw new BadRequestException('timeFrom must be before timeTo');
+      if (timeFrom > timeTo) throw new BadRequestException('timeFrom must be before timeTo');
       const oneDayInMs = BigInt(1000) * BigInt(60) * BigInt(60) * BigInt(24);
-      timeTo = Number(BigInt(timeFrom) + oneDayInMs - BigInt(1));
+      timeTo = Number(BigInt(timeTo) + oneDayInMs - BigInt(1));
+      console.log(timeFrom, timeTo);
     }
+    console.log(timeFrom, timeTo);
     // If either timeFrom or timeTo is missing, fetch all reservations for the space
     const reservations = await this.reservationRepository.fetch({ 
       spaceId, 
