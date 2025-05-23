6b82dfb533319d96b5b84794780e893aa41c8fb8
h1ghl1kh7 <kimhyilim.915@gmail.com>
2025-05-23 15:58:27 +0900

    UPDATE: back

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/parser',
   ],
   root: true,
   env: {
