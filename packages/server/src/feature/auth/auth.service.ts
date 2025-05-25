d52026b732965ece72339891ae4e57d6e130afad
h1ghl1kh7 <kimhyilim.915@gmail.com>
2025-05-25 23:53:56 +0900

    UPDATE: back

--- packages/server/src/feature/auth/auth.service.ts
+++ packages/server/src/feature/auth/auth.service.ts
@@ -19,16 +19,8 @@ export class AuthService {
   ) {}
 
   verifyState(state: number){
-    const v1 = this.configService.get<number>("SSO_STATE1");
-    const v2 = this.configService.get<number>("SSO_STATE2");
-    console.log("V1 " + v1);
-    console.log("V2 " + v2);
-    console.log(state ^ v1)
-    console.log("typeof")
-    console.log(typeof state)
-    console.log(typeof v1)
-    console.log(typeof (state ^ v1))
-    console.log((state ^ v1) === v2)
+    const v1 = parseInt(this.configService.get<string>("SSO_STATE1"));
+    const v2 = parseInt(this.configService.get<string>("SSO_STATE2"));
 
     if ((state ^ v1) === v2){
       return true;
@@ -37,8 +29,6 @@ export class AuthService {
   }
 
   async login(state: number, code: string): Promise<string> {
-    console.log("STATE " + state);
-    console.log("CODE " + code);
     if (!code) {
       throw new Error('No code provided');
     }
