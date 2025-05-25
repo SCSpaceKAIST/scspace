06534f021951c830b3207a3bc1cc54d51a2779c1
seohokim-hoya <seoho7777.kim@gmail.com>
2025-05-26 04:37:37 +0900

    UPDATE: front

--- packages/client/src/Components/Login/Eng.tsx
+++ packages/client/src/Components/Login/Eng.tsx
@@ -30,7 +30,6 @@ export function ENG() {
                 Last updated: May 25, 2025
             </Text>
             <Text>
-
                 This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
             </Text>
             <Text>
@@ -65,7 +64,7 @@ export function ENG() {
                         Affiliate
                     </DataList.ItemLabel>
                     <DataList.ItemValue>
-                        means an entity that controls, is controlled by or is under common control with a party, where "control" means ownership of 50% or more of the shares, equity interest or other securities entitled to vote for election of directors or other managing authority.
+                        means an entity that controls, is controlled by or is under common control with a party, where &quot;control&quot; means ownership of 50% or more of the shares, equity interest or other securities entitled to vote for election of directors or other managing authority.
                     </DataList.ItemValue>
                 </DataList.Item>
                 <DataList.Item>
@@ -73,7 +72,7 @@ export function ENG() {
                         Company
                     </DataList.ItemLabel>
                     <DataList.ItemValue>
-                        (referred to as either "the Company", "We", "Us" or "Our" in this Agreement) refers to KAIST Student Culture & Space Committee official website.
+                        (referred to as either the &quot;Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Agreement) refers to KAIST Student Culture & Space Committee official website.
                     </DataList.ItemValue>
                 </DataList.Item>
                 <DataList.Item>
@@ -183,7 +182,7 @@ export function ENG() {
                 Usage Data is collected automatically when using the Service.
             </Text>
             <Text>
-                Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.
+                Usage Data may include information such as Your Device&apos;s Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.
             </Text>
             <Text>
                 When You access the Service by or through a mobile device, We may collect certain information automatically, including, but not limited to, the type of mobile device You use, Your mobile device unique ID, the IP address of Your mobile device, Your mobile operating system, the type of mobile Internet browser You use, unique device identifiers and other diagnostic data.
@@ -216,7 +215,7 @@ export function ENG() {
                 </DataList.Item>
             </DataList.Root>
             <Text>
-                Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies remain on Your personal computer or mobile device when You go offline, while Session Cookies are deleted as soon as You close Your web browser. Learn more about cookies on the <Link href="https://www.freeprivacypolicy.com/blog/sample-privacy-policy-template/#Use_Of_Cookies_And_Tracking" target="_blank" color="blue">Free Privacy Policy website</Link> article.
+                Cookies can be &quot;Persistent&quot; or &quot;Session&quot; Cookies. Persistent Cookies remain on Your personal computer or mobile device when You go offline, while Session Cookies are deleted as soon as You close Your web browser. Learn more about cookies on the <Link href="https://www.freeprivacypolicy.com/blog/sample-privacy-policy-template/#Use_Of_Cookies_And_Tracking" target="_blank" color="blue">Free Privacy Policy website</Link> article.
             </Text>
             <Text>
                 We use both Session and Persistent Cookies for the purposes set out below:
@@ -316,7 +315,7 @@ export function ENG() {
                         To contact You:
                     </DataList.ItemLabel>
                     <DataList.ItemValue>
-                        To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application's push notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when necessary or reasonable for their implementation.
+                        To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application&apos;s push notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when necessary or reasonable for their implementation.
                     </DataList.ItemValue>
                 </DataList.Item>
                 <DataList.Item>
@@ -418,7 +417,7 @@ export function ENG() {
                 Transfer of Your Personal Data
             </H3>
             <Text>
-                Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.
+                Your information, including Personal Data, is processed at the Company&apos;s operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.
             </Text>
             <Text>
                 Your consent to this Privacy Policy followed by Your submission of such information represents Your agreement to that transfer.
@@ -496,19 +495,19 @@ export function ENG() {
                 The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.
             </Text>
             <H2>
-                Children's Privacy
+                Children&apos;s Privacy
             </H2>
             <Text>
                 Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.
             </Text>
             <Text>
-                If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.
+                If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent&apos;s consent before We collect and use that information.
             </Text>
             <H2>
                 Links to Other Websites
             </H2>
             <Text>
-                Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.
+                Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party&apos;s site. We strongly advise You to review the Privacy Policy of every site You visit.
             </Text>
             <Text>
                 We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.
@@ -520,7 +519,7 @@ export function ENG() {
                 We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy Policy on this page.
             </Text>
             <Text>
-                We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the "Last updated" date at the top of this Privacy Policy.
+                We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the &quot;Last updated&quot; date at the top of this Privacy Policy.
             </Text>
             <Text>
                 You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.
