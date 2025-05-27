f9232a399f77f0bc29dc3c4b56f73e7b0a11a8d1
seohokim-hoya <seoho7777.kim@gmail.com>
2025-05-27 12:15:31 +0900

    UPDATE: front

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -6,18 +6,12 @@ export default function SpacePage() {
       <VStack separator={<StackSeparator />}>
         <VStack px={16}>
           <Heading>
-            홈 화면은 개발중입니다.
-          </Heading>
-          <Heading>
-            사이드 메뉴를 이용하시길 바랍니다.
+            학생문화공간위원회
           </Heading>
         </VStack>
         <VStack px={16}>
           <Heading>
-            The home screen is under development.
-          </Heading>
-          <Heading>
-            Please use the side menu.
+            SCSpace
           </Heading>
         </VStack>
       </VStack>
