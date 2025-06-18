f3e51c5c2bb6620eecbd7a3e84eca633b650da51
h1ghl1kh7 <kimhyilim.915@gmail.com>
2025-06-19 02:30:16 +0900

    UPDATE: test

--- packages/server/src/feature/reservation/reservation.public.service.ts
+++ packages/server/src/feature/reservation/reservation.public.service.ts
@@ -185,6 +185,8 @@ export class ReservationPublicService {
     if (!space) {
       throw new BadRequestException('Space not found');
     }
+    
+    console.log(~~(getDateDiff((~~(getNow()/(60*24))*60*24), (~~(timeFrom/(60*24))*60*24))/(60*24)));
     // check min / max time
     if (reservationMinDate[space.spaceType] > ~~(getDateDiff((~~(getNow()/(60*24))*60*24), (~~(timeFrom/(60*24))*60*24))/(60*24))) {
       throw new BadRequestException(`Check the minimum reservation date. ${space.nameEn} can be reserved at least ${reservationMinDate[space.spaceType]} days in advance.`);
