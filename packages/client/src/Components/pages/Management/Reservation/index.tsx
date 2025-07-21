4ec5f0a0719f7ddd5a49a1debbafd4256a0008cb
seohokim-hoya <seoho7777.kim@gmail.com>
2025-07-21 22:22:44 +0900

    UPDATE: all res

--- packages/client/src/Components/pages/Management/Reservation/index.tsx
+++ packages/client/src/Components/pages/Management/Reservation/index.tsx
@@ -1,16 +1,17 @@
 import { Tabs } from "@chakra-ui/react";
 import Scroll from "@scspace-client/Components/molecules/page/Scroll";
+import AllReservation from "@scspace-client/Components/organisms/Reservation/Listing/AllReservation";
 import React from "react";
 
 export default function ManageReservation() {
     const tabList: { [key: string]: React.ReactNode } = {
-        List: <div>List of Reservations</div>,
+        List: <AllReservation />,
         Create: <div>Create a New Reservation</div>,
     };
 
     return (
         <Scroll>
-            <Tabs.Root defaultValue={Object.keys(tabList)[1]} fitted>
+            <Tabs.Root defaultValue={Object.keys(tabList)[0]} fitted>
                 <Tabs.List>
                     {Object.keys(tabList).map((key) => (
                         <Tabs.Trigger key={key} value={key}>
