b625dfa2566b4a7becff3b36ff186a115d0e339f
seohokim-hoya <seoho7777.kim@gmail.com>
2025-07-23 20:57:39 +0900

    UPDATE: join

--- packages/server/src/feature/reservation/reservation.repository.ts
+++ packages/server/src/feature/reservation/reservation.repository.ts
@@ -218,7 +218,7 @@ export class ReservationRepository {
       query = this.db
         .select()
         .from(Reservation)
-        .leftJoin(
+        .innerJoin(
           OrganizationMember,
           and(
             eq(OrganizationMember.organizationId, Reservation.organizationId),
