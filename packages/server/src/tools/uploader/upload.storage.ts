ec3571fa02187495b691ab0d345c9da8e152768c
seohokim-hoya <seoho7777.kim@gmail.com>
2025-08-29 01:29:28 +0900

    UPDATE: file uploader

---
+++ packages/server/src/tools/uploader/upload.storage.ts
@@ -0,0 +1,10 @@
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+export const fileStorage = diskStorage({
+    destination: './uploads/images', // 저장 폴더
+    filename: (req, file, cb) => {
+        const unique = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, `${unique}${extname(file.originalname)}`);
+    },
+});
