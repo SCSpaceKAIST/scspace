4af09e54a7c0d9f6e24209bbb019e14a836f8c78
seohokim-hoya <seoho7777.kim@gmail.com>
2025-08-31 03:53:43 +0900

    UPDATE: img viewer

--- packages/client/src/Components/organisms/Rental/GoodsList/GoodsListItem.tsx
+++ packages/client/src/Components/organisms/Rental/GoodsList/GoodsListItem.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Box, Button, CheckboxCard, Collapsible, Flex, Grid, Separator, Stack } from "@chakra-ui/react";
+import { Box, Button, CheckboxCard, CloseButton, Collapsible, Dialog, Flex, Grid, Separator, Stack } from "@chakra-ui/react";
 import { IGoods, IRentalCreateClient } from "@scspace-depot/types/rental";
 import Image from "next/image";
 import { useState } from "react";
@@ -66,75 +66,110 @@ export default function GoodsListItem({
         );
     }
 
+    const [imgOpen, setImgOpen] = useState(false);
+
     return (
-        <CheckboxCard.Root
-            _hover={{
-                backgroundColor: "white"
-            }}
-            key={item.id}
-            checked={isSelected}
-            onCheckedChange={(v) => {
-                onSelect(item.id);
-            }}
-        >
-            {!disabled && <CheckboxCard.HiddenInput />}
-            <Stack gap={0}>
-                <Grid
-                    templateColumns={isWide ? "auto auto 1fr" : "1fr"}
-                    templateRows={isWide ? "1fr" : "auto auto 1fr "}
-                >
-                    <Box
-                        aspectRatio={5 / 4}
-                        p={2}
+        <>
+            <Dialog.Root
+                open={imgOpen && isWide}
+                onOpenChange={(v) => setImgOpen(v.open)}
+                size="cover"
+                placement={"center"}
+            >
+                <Dialog.Backdrop zIndex={1500} />
+                <Dialog.Positioner zIndex={1600}>
+                    <Dialog.Content>
+                        <Dialog.CloseTrigger asChild zIndex={1700}>
+                            <CloseButton />
+                        </Dialog.CloseTrigger>
+                        <Dialog.Body p={2}>
+                            <Box
+                                position="relative"
+                                h={"full"}
+                                w={"full"}
+                            >
+                                <Image
+                                    src={"http://localhost:3001" + (item.imageURI || "/img/logo.svg")}
+                                    alt="LOGO"
+                                    fill
+                                    objectFit="contain"
+                                />
+                            </Box>
+                        </Dialog.Body>
+                    </Dialog.Content>
+                </Dialog.Positioner>
+            </Dialog.Root>
+            <CheckboxCard.Root
+                _hover={{
+                    backgroundColor: "white"
+                }}
+                key={item.id}
+                checked={isSelected}
+                onCheckedChange={(v) => {
+                    onSelect(item.id);
+                }}
+            >
+                {!disabled && <CheckboxCard.HiddenInput />}
+                <Stack gap={0}>
+                    <Grid
+                        templateColumns={isWide ? "auto auto 1fr" : "1fr"}
+                        templateRows={isWide ? "1fr" : "auto auto 1fr "}
                     >
                         <Box
-                            position="relative"
-                            h={"full"}
-                            w={"full"}
+                            aspectRatio={5 / 4}
+                            p={2}
                         >
-                            <Image
-                                src={"http://localhost:3001" + (item.imageURI || "/img/logo.svg")}
-                                alt="LOGO"
-                                fill
-                                objectFit="contain"
-                            />
+                            <Box
+                                position="relative"
+                                h={"full"}
+                                w={"full"}
+                                onClick={() => setImgOpen(true)}
+                                cursor={"pointer"}
+                            >
+                                <Image
+                                    src={"http://localhost:3001" + (item.imageURI || "/img/logo.svg")}
+                                    alt="LOGO"
+                                    fill
+                                    objectFit="contain"
+                                />
+                            </Box>
                         </Box>
-                    </Box>
-                    <Separator orientation={isWide ? "vertical" : "horizontal"} />
-                    <Stack gap={0}>
-                        <CheckboxCard.Control>
-                            <CheckboxCard.Content>
-                                <CheckboxCard.Label>
-                                    {item.name}
-                                </CheckboxCard.Label>
-                                <CheckboxCard.Description>
-                                    {item.countNow} / {item.countAll} Available
-                                </CheckboxCard.Description>
-                            </CheckboxCard.Content>
-                        </CheckboxCard.Control>
-                        {(item.description !== null) && (
-                            <CheckboxCard.Addon>
-                                {item.description}
-                            </CheckboxCard.Addon>
-                        )}
-                    </Stack>
-                </Grid>
-                <Collapsible.Root open={isSelected && !manage}>
-                    <Collapsible.Content>
-                        <Separator />
-                        <Flex justify={"flex-end"} p={2} gap={4}>
-                            <Counter
-                                count={count}
-                                setCount={setCount}
-                                max={countAvailable}
-                            />
-                            <Button size={"sm"} colorPalette={"blue"} onClick={handleCreate}>
-                                Confirm
-                            </Button>
-                        </Flex>
-                    </Collapsible.Content>
-                </Collapsible.Root>
-            </Stack>
-        </CheckboxCard.Root>
+                        <Separator orientation={isWide ? "vertical" : "horizontal"} />
+                        <Stack gap={0}>
+                            <CheckboxCard.Control>
+                                <CheckboxCard.Content>
+                                    <CheckboxCard.Label>
+                                        {item.name}
+                                    </CheckboxCard.Label>
+                                    <CheckboxCard.Description>
+                                        {item.countNow} / {item.countAll} Available
+                                    </CheckboxCard.Description>
+                                </CheckboxCard.Content>
+                            </CheckboxCard.Control>
+                            {(item.description !== null) && (
+                                <CheckboxCard.Addon>
+                                    {item.description}
+                                </CheckboxCard.Addon>
+                            )}
+                        </Stack>
+                    </Grid>
+                    <Collapsible.Root open={isSelected && !manage}>
+                        <Collapsible.Content>
+                            <Separator />
+                            <Flex justify={"flex-end"} p={2} gap={4}>
+                                <Counter
+                                    count={count}
+                                    setCount={setCount}
+                                    max={countAvailable}
+                                />
+                                <Button size={"sm"} colorPalette={"blue"} onClick={handleCreate}>
+                                    Confirm
+                                </Button>
+                            </Flex>
+                        </Collapsible.Content>
+                    </Collapsible.Root>
+                </Stack>
+            </CheckboxCard.Root>
+        </>
     );
 }
