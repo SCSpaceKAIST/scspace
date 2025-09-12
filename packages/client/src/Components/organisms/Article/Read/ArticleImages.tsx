4577beb6f514e24eaef99222d89248934611c21d
seohokim-hoya <seoho7777.kim@gmail.com>
2025-09-12 18:32:28 +0900

    UPDATE: Article Image design

--- packages/client/src/Components/organisms/Article/Read/ArticleImages.tsx
+++ packages/client/src/Components/organisms/Article/Read/ArticleImages.tsx
@@ -12,26 +12,27 @@ export default function ArticleImages({ images }: { images: string[] }) {
     return (
         <Center overflow={"hidden"} minH={0} w="100%" maxW="100%" whiteSpace="nowrap">
             <HStack w="fit-content" minWidth="max-content" h="450px">
-                {Array.from({ length: images.length }).map((_, idx) => (
-                    <Box
-                        key={`empty-${idx}-before`}
-                        w={idx < images.length - select - 1 ? "240px" : "0"}
-                        transition={"all"}
-                        transitionDuration={"moderate"}
-                    />
-                ))}
+                <Box
+                    key={`empty-before`}
+                    w={`${248 * (images.length - select - 1) + 8}px`}
+                    // h={'100px'}
+                    transition={"all"}
+                    transitionDuration={"moderate"}
+                // background={"gray.200"}
+                />
                 {images.map((image, idx) => (
                     <Box
                         w={(select === idx ? "360px" : "240px")}
                         h={(select === idx ? "450px" : "300px")}
-                        maxW={"full"}
+                        maxW={"72svw"}
+                        maxH={"96svw"}
                         key={`${image}-${idx}`}
                         justifyContent={"center"}
                         alignContent={"center"}
                         onClick={() => setSelect(idx)}
                         transition={"all"}
                         transitionDuration={"moderate"}
-                        background={"gray.50"}
+                        background={(select !== idx ? "gray.200" : "transparent")}
                         position={"relative"}
                     >
                         <Image
@@ -42,14 +43,14 @@ export default function ArticleImages({ images }: { images: string[] }) {
                         />
                     </Box>
                 ))}
-                {Array.from({ length: images.length }).map((_, idx) => (
-                    <Box
-                        key={`empty-${idx}-after`}
-                        w={idx < select ? "240px" : "0"}
-                        transition={"all"}
-                        transitionDuration={"moderate"}
-                    />
-                ))}
+                <Box
+                    key={`empty-after`}
+                    w={`${248 * (select) + 8}px`}
+                    // h={'100px'}
+                    transition={"all"}
+                    transitionDuration={"moderate"}
+                // background={"gray.200"}
+                />
             </HStack>
         </Center>
     );
--- packages/client/src/app/article/[id]/page.tsx
+++ packages/client/src/app/article/[id]/page.tsx
@@ -1,15 +1,9 @@
-import PageTemplete from "@scspace-client/Components/molecules/page/PageTemplete";
 import ArticleDetail from "@scspace-client/Components/pages/Article/Detail";
 
 export default async function ArticleDetailPage({ params }: { params: Promise<{ id: number }> }) {
     const { id } = await params;
 
     return (
-        <PageTemplete
-            title={["게시판", "상세보기"]}
-            subtitle={["Article", "Detail"]}
-        >
-            <ArticleDetail id={id} />
-        </PageTemplete>
+        <ArticleDetail id={id} />
     );
 }
