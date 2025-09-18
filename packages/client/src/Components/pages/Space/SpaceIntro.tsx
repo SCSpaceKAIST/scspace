6487adaa360f7c6937fe7a196a33695a90af8b46
seohokim-hoya <seoho7777.kim@gmail.com>
2025-09-18 17:13:05 +0900

    UPDATE: browse/spaces

--- packages/client/src/Components/pages/Space/SpaceIntro.tsx
+++ packages/client/src/Components/pages/Space/SpaceIntro.tsx
@@ -1,24 +1,44 @@
-"use clien"
+"use client"
 
-import { Center, Heading, StackSeparator, VStack } from "@chakra-ui/react";
+import { Box, Center, Grid, Heading, Mark, Stack, Text } from "@chakra-ui/react";
 import { ISpace } from "@scspace-depot/types/space";
+import Image from "next/image";
+import { SpaceInfo, SpaceInfoMatcher } from "@scspace-depot/consts/space.const";
+import { useEffect, useState } from "react";
+import LoadingComponent from "@scspace-client/Components/atoms/Loading";
 
 export default function SpaceIntro({ space }: { space: ISpace }) {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [space.id]);
 
   return (
-    <Center height="100%">
-      <VStack separator={<StackSeparator />}>
-        <VStack px={16}>
-          <Heading>
-            {space.nameKr}
-          </Heading>
-        </VStack>
-        <VStack px={16}>
-          <Heading>
-            {space.nameEn}
-          </Heading>
-        </VStack>
-      </VStack>
-    </Center>
+    <Grid templateRows="auto auto 1fr" h={"full"} gap={4}>
+      <Heading>
+        {space.nameKr} <br />
+        <Mark color={"gray"}>{space.nameEn}</Mark>
+      </Heading>
+      <Center aspectRatio={5 / 4}>
+        <Box
+          position="relative"
+          h={"full"}
+          w={"full"}
+        >
+          {!loaded && <LoadingComponent />}
+          <Image
+            src={`/img/spaces/${SpaceInfo[SpaceInfoMatcher[space.id]].img}.jpg`}
+            alt="LOGO"
+            fill
+            objectFit="contain"
+            onLoad={() => setLoaded(true)}
+          />
+        </Box>
+      </Center>
+      <Text>
+        {SpaceInfo[SpaceInfoMatcher[space.id]].desc}
+      </Text>
+    </Grid>
   );
 }
