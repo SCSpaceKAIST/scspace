1c7fd288da1366fe550d3ad279ce284b6856b447
seohokim-hoya <seoho7777.kim@gmail.com>
2025-11-02 02:48:45 +0900

    Feat [Passpin]: Auth

--- packages/client/src/Components/pages/Passpin/index.tsx
+++ packages/client/src/Components/pages/Passpin/index.tsx
@@ -1,5 +1,11 @@
+"use client"
+
 import PasspinView from "@scspace-client/Components/organisms/Passpin/Read/PasspinView";
+import { useAuth } from "@scspace-client/Hooks/auth";
 
 export default function PasspinPage() {
+    const { needPasspinMaster } = useAuth();
+    needPasspinMaster();
+
     return <PasspinView />;
 }
