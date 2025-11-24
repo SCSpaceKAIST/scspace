bbd99671006caa6063abf6c051078f7cf85375bb
hyilim <hyilim@hyilimui-MacBookPro.local>
2025-11-25 02:28:18 +0900

    UPDATE: debugger

--- packages/server/src/feature/auth/jwt/jwt.guard.ts
+++ packages/server/src/feature/auth/jwt/jwt.guard.ts
@@ -185,6 +185,7 @@ export class MemberGuardWithReservation extends AuthGuard('jwt') {
       return true;
     }
 
+    this.logger.log(`ID: ${request.params}`);
     const id = parseInt(request.params.id);
     const reservation = await this.reservationPublicService.fetchById(id);
     if (reservation === null) {
