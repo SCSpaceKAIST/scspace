f288ee16dcec52de4ee57a03762949410ab432de
seohokim-hoya <seoho7777.kim@gmail.com>
2025-11-25 02:44:27 +0900

    Feat [Rental]: Block rental

--- packages/client/src/Components/pages/Rental/Application/index.tsx
+++ packages/client/src/Components/pages/Rental/Application/index.tsx
@@ -7,10 +7,13 @@ import { useAuth } from "@scspace-client/Hooks/auth";
 export default function RentalApplication() {
     const { isLogined } = useAuth();
 
+    alert("Rental Application page is under update.");
+
     return (
         <Scroll>
             <GoodsList
-                disabled={!isLogined}
+                // disabled={!isLogined}
+                disabled
             />
         </Scroll>
     );
